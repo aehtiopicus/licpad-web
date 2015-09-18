44b384d2ee3d4696d7dd652a6a01acf83c30f9ee
aehtiopicus <aehtiopicus@gmail.com>
2015-09-18 02:21:25 -0300

    adding changes

---
+++ licpad-web/src/main/java/com/aehtiopicus/licpad/config/PersistenceContext.java
@@ -0,0 +1,95 @@
+package com.aehtiopicus.licpad.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate4.HibernateExceptionTranslator;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages="com.aehtiopicus.licpad.repository")
+public class PersistenceContext {
+
+	@Bean(name ="entityManagerFactory")
+	public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		factoryBean.setDataSource(restDataSource());
+		factoryBean
+				.setPackagesToScan(new String[] { "com.aehtiopicus.licpad.domain" });
+
+		JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter() {
+
+			{
+				// JPA properties
+				setShowSql(true);
+				setDatabase(Database.DERBY);
+				setGenerateDdl(true);
+			}
+		};
+		factoryBean.setJpaVendorAdapter(vendorAdapter);
+		factoryBean.setJpaProperties(additionalProperties());
+
+		return factoryBean;
+	}
+
+	@Bean(name = "dataSource")
+	public DataSource restDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.apache.derby.jdbc.EmbeddedDriver");
+		dataSource.setUrl("jdbc:derby:db/LICCPaDDB;create=true");
+		dataSource.setUsername("lw");
+		dataSource.setPassword("lw");		
+		return dataSource;
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactoryBean()
+				.getObject());
+
+		return transactionManager;
+	}
+
+	@Bean
+	public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
+		return new PersistenceExceptionTranslationPostProcessor();
+	}
+
+	@Bean 
+	public HibernateExceptionTranslator hibernateExceptionTranslator(){ 
+		return new HibernateExceptionTranslator(); 
+	}
+	 
+
+	final Properties additionalProperties() {
+		return new Properties() {
+
+			/**
+             *
+             */
+			private static final long serialVersionUID = 3996171551371239499L;
+
+			{
+				// use this to inject additional properties in the EntityManager
+				setProperty("hibernate.hbm2ddl.auto", "update");
+				setProperty("hibernate.show_sql","true");
+				setProperty("hibernate.dialect","hibernate.dialect");
+				setProperty("hibernate.format_sql","true");
+			}
+		};
+	}
+}
